23d0b3344d839c6ace8078474724660bb3836e98
unknown <sishaile@USCTCOVM6668.MERCK.com>
2013-12-31 10:35:26 -0500

    initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webops::Application.config.secret_token = 'a89776648112aefbc92d4d0c141ae4d6db357b2af9e86eea37ec275a87ab9d3c00e3418d934b717e14a4ccfd9783b2b558e5f4f1a5cf8ca545ccaf23d0a0f2dd'
